df9f22d86dfd0026494dfbe2a40d0e74ad2300a9
USUARIO <USUARIO@DESKTOP-5TO22FJ>
2023-12-13 10:23:02 +0100

    Ejercicios

---
+++ src/main/java/es/curso/java/herencia/ArrancaHospital.java
@@ -0,0 +1,10 @@
+package es.curso.java.herencia;
+
+public class ArrancaHospital {
+
+	public static void main(String[] args) {
+		Hospital hospital1=new Hospital("Almenara");
+		hospital1.abrirHospital();
+
+	}
+}
